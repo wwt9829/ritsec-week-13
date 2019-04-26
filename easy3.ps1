2fcffb9d70d61e338fe7388678815f96a24b32f9
Wyatt Tauber <wwt9829@g.rit.edu>
2019-04-26 10:08:08 -0400

    Right location for easy3

---
+++ easy3.ps1
@@ -0,0 +1,7 @@
+﻿# RITSEC Demos Week 13
+# Easy 3
+
+New-NetIPAddress -InterfaceIndex 11 -IPAddress 192.168.1.1 -PrefixLength "24" -DefaultGateway 192.168.1.254
+Set-DnsClientServerAddress -InterfaceIndex 11 -ServerAddresses 192.168.1.1
+Install-WindowsFeature AD-Domain-Services
+Set-ADDomain -Identity DHCP1 -AllowedDNSSuffixes @{Add="ritsec.com"}
